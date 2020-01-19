c2df0fcdc256ec059589fd25c0e96c3eaee26b4b
IKarbowiak <idzi.karbowiak@gmail.com>
2020-01-19 10:42:46 +0100

    Add tests for delete book mutations

---
+++ tests/book/test_book_mutation.py
@@ -0,0 +1,66 @@
+import graphene
+from bookies.schema import schema
+from graphene.test import Client
+
+BOOK_DELETE_MUTATION = """
+    mutation BookDelete($id: ID!){
+            bookDelete(id: $id){
+                book{
+                    id
+                    title
+                    author{
+                        name
+                    }
+                    description
+                    cover
+                }
+            }
+        }
+"""
+
+
+def test_delete_book_mutation(book):
+    client = Client(schema)
+    variables = {"id": graphene.Node.to_global_id("Book", book.pk)}
+    response = client.execute(BOOK_DELETE_MUTATION, variables=variables)
+
+    data = response["data"]["bookDelete"]["book"]
+
+    assert data["title"] == book.title
+    assert data["author"]["name"] == book.author.name
+
+
+USER_BOOK_DELETE_MUTATION = """
+    mutation UserBookDelete($id: ID!){
+            userBookDelete(id: $id){
+                userBook{
+                    status
+                    rate
+                    book{
+                        title
+                        author{
+                            name
+                        }
+                        description
+                        cover
+                    }
+                }
+            }
+        }
+"""
+
+
+def test_delete_user_book_mutation(user_to_book, rq_authenticated):
+    book = user_to_book.book
+
+    client = Client(schema)
+    variables = {"id": graphene.Node.to_global_id("UserToBook", user_to_book.pk)}
+    response = client.execute(
+        USER_BOOK_DELETE_MUTATION, variables=variables, context=rq_authenticated
+    )
+
+    data = response["data"]["userBookDelete"]["userBook"]
+
+    assert data["book"]["title"] == book.title
+    assert data["book"]["author"]["name"] == book.author.name
+    assert data["status"] == user_to_book.status.name
---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from book.models import Author, Book, BookStatuses, UserToBook
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.sessions.middleware import SessionMiddleware
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create(username="Test user", password="Test")
+
+
+@pytest.fixture
+def rq_authenticated(rf, user):
+    request = rf.post("/")
+    SessionMiddleware().process_request(request)
+    request.session.save()
+    request.user = user
+
+    return request
+
+
+@pytest.fixture
+def rq_anonymous(rf):
+    request = rf.post("/")
+    SessionMiddleware().process_request(request)
+    request.session.save()
+    request.user = AnonymousUser()
+
+    return request
+
+
+@pytest.fixture
+def author(db):
+    return Author.objects.create(name="Sample author")
+
+
+@pytest.fixture
+def book(author):
+    return Book.objects.create(
+        title="Sample book",
+        author=author,
+        year=1900,
+        description="Sample description",
+        number_of_pages=100,
+    )
+
+
+@pytest.fixture
+def user_to_book(user, book):
+    return UserToBook.objects.create(
+        user=user, book=book, rate=5, status=BookStatuses.READ
+    )
