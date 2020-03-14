735dc1ba46118ff3fd882e07f32d0477451ac46d
IKarbowiak <idzi.karbowiak@gmail.com>
2020-03-14 12:33:25 +0100

    Add tests for books query filters

--- tests/book/test_book_queries.py
+++ tests/book/test_book_queries.py
@@ -1,9 +1,10 @@
 import graphene
-from book.models import Book, UserToBook
+import pytest
+from book.models import Author, Book, UserToBook
 
 BOOKS_QUERY = """
-    query Books($author: String, $orderBy: String){
-        books(author: $author, orderBy: $orderBy) {
+    query Books($orderBy: String){
+        books(orderBy: $orderBy) {
             edges {
             node {
                 id
@@ -15,19 +16,152 @@ BOOKS_QUERY = """
 """
 
 
-def test_books_query(book, client):
+def test_books_query_order_by_year(book, client):
     book2 = Book.objects.get(pk=book.pk)
     book2.pk = None
     book2.title = book.title + " Second part"
     book2.year = book.year - 50
     book2.save()
 
-    variables = {"author": "Sample", "orderBy": "-year"}
+    variables = {"orderBy": "-year"}
     response = client.execute(BOOKS_QUERY, variables=variables)
 
     result = response["data"]["books"]["edges"]
     assert len(result) == 2
     assert [node["node"]["title"] for node in result] == [book.title, book2.title]
+
+
+def test_books_query_order_by_title(book, client):
+    book2 = Book.objects.get(pk=book.pk)
+    book2.pk = None
+    book2.title = "Book"
+    book2.year = book.year - 50
+    book2.save()
+
+    variables = {"orderBy": "title"}
+    response = client.execute(BOOKS_QUERY, variables=variables)
+
+    result = response["data"]["books"]["edges"]
+    assert len(result) == 2
+    assert [node["node"]["title"] for node in result] == [book2.title, book.title]
+
+
+def test_books_query_author_filter(author, book, client):
+    query = """
+        query Books($author: String){
+            books(author: $author) {
+                edges {
+                    node {
+                        id
+                        title
+                        author {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    """
+    author2 = Author.objects.create(name="Example")
+
+    book2 = Book.objects.get(pk=book.pk)
+    book2.pk = None
+    book2.title = "Book"
+    book2.author = author2
+    book2.year = book.year - 50
+    book2.save()
+
+    variables = {"author": author2.name}
+    response = client.execute(query, variables=variables)
+
+    result = response["data"]["books"]["edges"]
+    assert len(result) == 1
+    assert result[0]["node"]["title"] == book2.title
+
+
+def test_books_query_title_filter(book, client):
+    query = """
+        query Books($title: String){
+            books(title: $title) {
+                edges {
+                    node {
+                        id
+                        title
+                    }
+                }
+            }
+        }
+    """
+
+    book2 = Book.objects.get(pk=book.pk)
+    book2.pk = None
+    book2.title = "Book"
+    book2.year = book.year - 50
+    book2.save()
+
+    variables = {"title": "boo"}
+    response = client.execute(query, variables=variables)
+
+    result = response["data"]["books"]["edges"]
+    assert len(result) == 2
+    assert {node["node"]["title"] for node in result} == {book2.title, book.title}
+
+
+def test_books_query_year_filter(book, client):
+    query = """
+        query Books($year: Float){
+            books(year: $year) {
+                edges {
+                    node {
+                        id
+                        title
+                    }
+                }
+            }
+        }
+    """
+    book2 = Book.objects.get(pk=book.pk)
+    book2.pk = None
+    book2.title = "Book"
+    book2.year = book.year - 50
+    book2.save()
+
+    variables = {"year": book.year}
+    response = client.execute(query, variables=variables)
+
+    result = response["data"]["books"]["edges"]
+    assert len(result) == 1
+    assert result[0]["node"]["title"] == book.title
+
+
+@pytest.mark.parametrize(
+    "year_gte, year_lte, count",
+    [(1900, None, 2), (1920, None, 1), (1850, 1920, 1), (None, 2000, 2)],
+)
+def test_books_query_year_gte_and_lte_filter(book, client, year_gte, year_lte, count):
+    query = """
+        query Books($yearGte: Float, $yearLte: Float){
+            books(yearGte: $yearGte, yearLte: $yearLte) {
+                edges {
+                    node {
+                        id
+                        title
+                    }
+                }
+            }
+        }
+    """
+    book2 = Book.objects.get(pk=book.pk)
+    book2.pk = None
+    book2.title = "Book"
+    book2.year = book.year + 50
+    book2.save()
+
+    variables = {"yearGte": year_gte, "yearLte": year_lte}
+    response = client.execute(query, variables=variables)
+
+    result = response["data"]["books"]["edges"]
+    assert len(result) == count
 
 
 BOOK_QUERY = """
